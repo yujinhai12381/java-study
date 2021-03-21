36b37aa0cb6f4f05c0db8cf90ab5e063c798d137
yujinhai <yujinhai@cmcm.com>
2021-03-21 22:05:43 +0800

    init

---
+++ study001/src/main/java/com/jinhai/Study001Application.java
@@ -0,0 +1,13 @@
+package com.jinhai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Study001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Study001Application.class, args);
+    }
+
+}
---
+++ study001/study002/src/main/java/com/jinhai/Study002Application.java
@@ -0,0 +1,13 @@
+package com.jinhai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Study002Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Study002Application.class, args);
+    }
+
+}
