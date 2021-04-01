f39f7c52c08e0deaf7d873d5db2b2d5cbb3fd2a7
yujinhai <yujinhai@cmcm.com>
2021-04-02 01:09:03 +0800

    study 002

---
+++ springcloud/cloud-consumer-order/src/main/java/com/jinhai/springcloud/order/OrderMain.java
@@ -0,0 +1,12 @@
+package com.jinhai.springcloud.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+public class OrderMain {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderMain.class,args);
+    }
+}
