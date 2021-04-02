7d6b220287bf1c8fae06d6b0f9158d17f59c5a8e
yujinhai <yujinhai@cmcm.com>
2021-04-02 11:04:00 +0800

    study-04-02

--- springcloud/cloud-provide-payment/src/main/java/com/jinhai/springcloud/controller/PaymentController.java
+++ springcloud/cloud-provide-payment/src/main/java/com/jinhai/springcloud/controller/PaymentController.java
@@ -7,8 +7,8 @@ import com.jinhai.springcloud.service.PaymentService;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.cloud.client.ServiceInstance;
-//import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
