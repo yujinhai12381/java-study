e2024a7ce0c253ef55328b121c71d81e58434be9
yujinhai <yujinhai@cmcm.com>
2021-04-02 12:40:45 +0800

    Eureka study

--- springcloud/cloud-consumer-order/src/main/java/com/jinhai/springcloud/order/OrderMain.java
+++ springcloud/cloud-consumer-order/src/main/java/com/jinhai/springcloud/order/OrderMain.java
@@ -2,9 +2,10 @@ package com.jinhai.springcloud.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain.class,args);
--- springcloud/cloud-provide-payment/src/main/java/com/jinhai/springcloud/PayMentMain.java
+++ springcloud/cloud-provide-payment/src/main/java/com/jinhai/springcloud/PayMentMain.java
@@ -4,9 +4,10 @@ package com.jinhai.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PayMentMain {
     public static void main(String[] args) {
         SpringApplication.run(PayMentMain.class, args);
