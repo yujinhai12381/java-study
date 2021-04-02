956ff8d7b909cf0bab63641432434c01847ec9f1
yujinhai <yujinhai@cmcm.com>
2021-04-02 12:49:24 +0800

    Eureka study add server port

--- springcloud/cloud-provide-payment/src/main/java/com/jinhai/springcloud/controller/PaymentController.java
+++ springcloud/cloud-provide-payment/src/main/java/com/jinhai/springcloud/controller/PaymentController.java
@@ -7,6 +7,7 @@ import com.jinhai.springcloud.service.PaymentService;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
@@ -22,15 +23,18 @@ public class PaymentController {
     @Autowired
     private PaymentService paymentService;
 
+    @Value("${server.port}")
+    private int serverPort;
+
+
     @ResponseBody
     @PostMapping("/payment/create")
     public CommonResult create(@RequestBody Payment dept){
         log.info("save dept: {}", dept);
         int i = paymentService.create(dept);
-        log.info("***************插入成功*******"+i);
         if(i>0){
-            //return new CommonResult(200,"插入数据库成功"+serverPort,i);
-            return new CommonResult(200,"插入数据库成功",i);
+            return new CommonResult(200,"插入数据库成功"+this.serverPort,i);
+            //return new CommonResult(200,"插入数据库成功",i);
         }else{
             return new CommonResult(444,"插入数据库失败",null);
         }
@@ -42,8 +46,7 @@ public class PaymentController {
         Payment payment = paymentService.queryById(id);
         log.info("***************查询成功*********"+payment);
         if(payment!=null){
-            //return new CommonResult(200,"查询成功"+serverPort,payment);
-            return new CommonResult(200,"查询成功",payment);
+            return new CommonResult(200,"查询成功"+this.serverPort,payment);
         }else{
             return new CommonResult(444,"查询失败",null);
         }
